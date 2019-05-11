834c81e364c4fec3480cf7b1a65c675b61beb455
bellagrunt <mattbellantoni3@gmail.com>
2019-05-11 10:36:12 +0000

    editing pandas

--- data-science/pandas.py
+++ data-science/pandas.py
@@ -1,24 +1,29 @@
-# upload csv | working with jupyter labs
-# Loading data with pandas
+# # upload csv | working with jupyter labs
+# # Loading data with pandas
 
+# # import pandas
 # import pandas
-import pandas
 
-# stores the path as a csv
-csv_path = '/home/../rogue-data/CRM-Eleven.csv'
+# # stores the path as a csv
+# csv_path = 'CRM-Eleven.csv'
 
-# df - dataframe
-df = pandas.read_csv(csv_path)
+# # df - dataframe
+# df = pandas.read_csv(csv_path)
 
-# or
-df = pd.read_csv(csv_path)
-df.head()
+# # or
+# df = pd.read_csv(csv_path)
+# df.head()
 
-# ---------------
-# accessing different parts of the database:
-df['Name']
-# command will show all the data under the 'Name' column
-df['Name'].unique()
-# command will show all the unique data in name
-df['Name'] >= 2018
-# selecting a piece of data within a few rows
+# # ---------------
+# # accessing different parts of the database:
+# df['Name']
+# # command will show all the data under the 'Name' column
+# df['Name'].unique()
+# # command will show all the unique data in name
+# df['Name'] >= 2018
+# # selecting a piece of data within a few rows
+
+
+# https://dataplatform.cloud.ibm.com/docs/content/wsj/analyze-data/project-lib-python.html
+# https://developer.ibm.com/tutorials/create-interactive-dashboards-on-watson-studio/
+# https://developer.ibm.com/tutorials/create-interactive-dashboards-on-watson-studio/
